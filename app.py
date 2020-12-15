01a7981e71f2c4fd68ba93cb2967efeb484054b4
eugenio <s236883@studenti.polito.it>
2020-12-16 00:43:17 +0100

    Test 3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/index')
+@app.route('/homepage')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/signin')
+def login():
+    return render_template('signin.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run()
