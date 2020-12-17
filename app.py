20b0dbfb1942923baffc02519e14e2b0b182c8ed
AleixCar <75929478+AleixCar@users.noreply.github.com>
2020-12-17 04:40:38 +0100

    Aleix Test 2

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kncjdiejdsfmsdasldfjwqop'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///website.db'
+app.config['SQLALCHEMY_COMMIT_TEARDOWN']=True
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+from model import User, Role
+from form import ConsumerRegForm, loginForm
+
 
 
 @app.route('/')
@@ -12,13 +24,64 @@ def hello_world():
 def homepage():
     return render_template('index.html')
 
+
+@app.before_first_request
+def setup():
+    db.drop_all()
+    db.create_all()
+    role_supplier = Role(name='Supplier')
+    role_consumer = Role(name='Consumer')
+    role_admin = Role(name='Admin')
+    db.session.add_all([role_supplier,role_consumer,role_admin])
+    db.session.commit()
+
+
+@app.route('/registerdb',methods=['POST','GET'])
+def regiterPagedb():
+    name=None
+    registerForm=ConsumerRegForm()
+    if registerForm.validate_on_submit():
+        name=registerForm.name.data
+        session['name']=registerForm.name.data
+        session['email']=registerForm.email.data
+        password_2 = bcrypt.generate_password_hash(registerForm.password.data).encode('utf-8')
+        newuser = User(name=registerForm.name.data,
+                       username=registerForm.email.data,
+                       password=registerForm.password.data, role_id=2)
+        db.session.add(newuser)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register-db.html',registerForm=registerForm,name_website='SQL Registration to IS 2020 Platform',name=name)
+
+
+
+
 @app.route('/signin')
 def login():
     return render_template('signin.html')
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
+# Consumer registration in db
+@app.route('/register', methods=['POST', 'GET'])
+def consumer_reg():
+    name=None
+    registerForm=ConsumerRegForm()
+    if registerForm.validate_on_submit():
+        session['name']=registerForm.name.data
+        session['email']=registerForm.email.data
+        user_info = User(name=registerForm.name.data,
+                        username=registerForm.email.data,
+                        password=registerForm.password.data,
+                        role_id=2  #Consumer is 2nd role
+                        )
+        db.session.add(user_info)
+        db.session.commit()
+
+
+        # name=registerForm.name.data
+    return render_template('register.html', registerForm=registerForm, name=name)
+
+
+
 
 if __name__ == '__main__':
     app.run()
---
+++ model.py
@@ -0,0 +1,20 @@
+from app import db
+
+class User (db.Model):
+    id=db.Column(db.Integer,primary_key=True) # Auto-incrementing
+    username=db.Column(db.String(50),unique=True,nullable=False)
+    name=db.Column(db.String(50),nullable=False)
+    password=db.Column(db.String(200),nullable=False)
+    role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
+
+    def __repr__(self):
+        return "<User %r>" % self.name
+
+
+class Role (db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20),nullable=False)
+    users=db.relationship('User',backref='role')
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
