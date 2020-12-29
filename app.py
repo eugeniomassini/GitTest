91571156d53917a6182dcb9741a21eac40d286c6
elisavassallo <75758407+elisavassallo@users.noreply.github.com>
2020-12-29 22:22:38 +0100

    form info written in tables with correct id

--- app.py
+++ app.py
@@ -82,13 +82,15 @@ def consumer_reg():
                         email=registerForm.email.data,
                         password=password_2,
                         roleid=2)
-        new_consumer = Consumer(consumer_email=registerForm.email.data,
+        db.session.add(new_user)
+        db.session.commit()
+        user_info = User.query.filter_by(email=registerForm.email.data).first()
+        session['user_id'] = user_info.id
+        new_consumer = Consumer(id=user_info.id,
                                 consumer_name=registerForm.name.data,
                                 consumer_surname=registerForm.familyname.data,
                                 consumer_address=registerForm.address.data,
                                 consumer_phone=registerForm.phone.data)
-        #TODO insert information also in consumer's table
-        db.session.add(new_user)
         db.session.add(new_consumer)
         db.session.commit()
 
@@ -118,14 +120,16 @@ def supplier_reg():
                          email=registerForm.email.data,
                          password=password_2,
                          roleid=1)
-        new_supplier = Supplier(supplier_email=registerForm.email.data,
+        db.session.add(user_info)
+        db.session.commit()
+        user_info = User.query.filter_by(email=registerForm.email.data).first()
+        session['user_id'] = user_info.id
+        new_supplier = Supplier(id=user_info.id,
                                 supplier_name=registerForm.name.data,
                                 supplier_address=registerForm.address.data,
                                 supplier_phone=registerForm.phone.data,
                                 piva=registerForm.piva.data,
                                 description=registerForm.description.data)
-        #TODO insert information in supplier's table
-        db.session.add(user_info)
         db.session.add(new_supplier)
         db.session.commit()
 
@@ -153,7 +157,6 @@ def login():
             session['email'] = user_info.email
             session['role_id'] = user_info.role_id
         return redirect('dashboard')  # TODO create dashboard.html
-
     return render_template('signin.html', login_form=login_form)
 
 
